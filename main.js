075290912ca564b279ae899cf7eb999732bae3fc
cmdblox <94880442+cmdblox@users.noreply.github.com>
2024-05-12 00:24:03 -0700

    added an easy location for functions

---
+++ main.js
@@ -0,0 +1,13 @@
+//this script is for functions for mods and other patches that don't need to directly modify the gamescript
+
+var functions = window._.functions;
+
+functions.dumpInversify = function(){
+    var objects = []
+    _.game._rootContainer.sourceContainer._bindingDictionary._map.forEach(function(e){
+        if(e[0]['implementationType']){
+            objects.push([e[0]['implementationType'].prototype, e[0]['guid'], e[0]['serviceIdentifier']])
+        }
+    })
+    return objects
+}
